2cc5b4860fc32c833ca3181263bba8120e534056
Bikila <nuwaliin_2008@yahoo.com>
2023-03-22 23:22:51 +0100

    variables added

--- infra/variables.tf
+++ infra/variables.tf
@@ -25,3 +25,34 @@ variable "ecr_repository_name" {
 description = "The name of the ECR repository."
 default = "ghost"
 }
+
+variable "app_name" {
+  description = "The name of the application."
+}
+
+variable "environment" {
+  description = "The name of the environment (e.g. prod, staging, dev)."
+}
+
+variable "db_password" {
+  description = "The password for the MySQL database user."
+  sensitive   = true
+}
+
+variable "smtp_password" {
+  description = "The password for the SMTP email account."
+  sensitive   = true
+}
+
+variable "github_repository_owner" {
+  description = "The owner of the GitHub repository to use for CodePipeline."
+}
+
+variable "github_repository_name" {
+  description = "The name of the GitHub repository to use for CodePipeline."
+}
+
+variable "github_branch" {
+  description = "The GitHub branch to use for CodePipeline."
+  default     = "main"
+}
